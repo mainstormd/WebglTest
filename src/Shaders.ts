5b8e37fc064e85397e16b29acb42b758c9898daf
mainstorm <krysav79@yandex.ru>
2023-12-24 11:29:26 +0300

    Add new position aproximate

--- src/Shaders.ts
+++ src/Shaders.ts
@@ -18,7 +18,7 @@ export const VERTEX_SHADER_SOURCE = `
         if(isSphere == true)
         {
             vec4 spherePosition = vec4(normalize(a_Position.xyz) * 3.0, a_Position.w);
-            position = mix(a_Position, spherePosition, (sin(time) + 1.0)/2.0);  
+            position = mix(a_Position, spherePosition, (sin(time)+(sin(3.0*time) / 3.0)  + 1.0)/2.0);  
         } else {
             position = a_Position;
         }
