cb79e98025dadccfb89b3c7fbed8640f1af1f57e
Dmytrii <krysav79@yandex.ru>
2024-10-02 19:51:02 +0300

    fix spaces

--- src/Utils/IndexBufferHelper.ts
+++ src/Utils/IndexBufferHelper.ts
@@ -4,7 +4,10 @@ export class IndexBufferHelper{
     // [0, 1, 2, 0, 2, 3] => [ 0, 1, 1, 2, 2, 0, 0, 2, 2, 3, 3, 0] 
     public static GetIdexesForRenderModeLines(indexesOld : number[])
     {
-        let indexes = indexesOld.reduce((reducer : number [], item : number) => { reducer.push(item,item); return reducer },[])
+        let indexes = indexesOld.reduce((reducer : number [], item : number) => { 
+            reducer.push(item,item); 
+            return reducer 
+        },[])
         let firstItem : number | undefined = indexes.shift()
         
         if(firstItem != null)
